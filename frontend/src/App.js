3d806c8f7bc7127ee63c749e7a76d519a27f1a9e
Michael Anthony Ballard <MichaelBallard@Michaels-MBP-2.fios-router.home>
2019-04-29 01:47:03 -0400

    built out ui basics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container, Row, Col } from 'reactstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            notes: [],
+            current_note_id: 0,
+            is_creating: true
+        }
+    }
+
+
+  render() {
+
+    return(
+        <React.Fragment>
+            <Container>
+                <Row>
+                    <Col xs="10">
+                        <h2>Realtime Trading Notes</h2>
+                    </Col>
+                    <Col xs="2">
+                        <Button color="success">Create Note</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs="4">
+                        <h5>List our notes here</h5>
+                    </Col>
+                    <Col xs="8">
+                        <p> Edit content here</p>
+                    </Col>
+
+
+                </Row>
+
+            </Container>
+
+        </React.Fragment>
+
+    )
+
+   };
 }
 
 export default App;
